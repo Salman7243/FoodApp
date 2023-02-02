90c17f261b5e0551d1bcec2bab3fee427f86778b
Salman7243 <98895288+Salman7243@users.noreply.github.com>
2023-02-01 16:44:58 -0800

    Add files via upload
    today

---
+++ lib/model/category_item_data_model.dart
@@ -0,0 +1,7 @@
+class CategoryItemDataModel{
+  final String image;
+  final String title;
+
+  CategoryItemDataModel({required this.image, required this.title});
+
+}
---
+++ lib/model/onboarding_model.dart
@@ -0,0 +1,9 @@
+
+
+class OnBoardingDataModel{
+  final String image;
+  final String title;
+  final String description;
+
+  OnBoardingDataModel({required this.image, required this.title, required this.description});
+}
